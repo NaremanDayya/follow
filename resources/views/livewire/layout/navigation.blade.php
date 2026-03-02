732b9514c9c8e526d907d574cd215a83cafafdf8
NaremanDayya <nanadaya166@gmail.com>
2026-03-02 10:58:48 +0200

    navigation update

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -33,7 +33,6 @@ new class extends Component
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
                 <div class="hidden space-x-1 sm:-my-px sm:ms-10 sm:flex items-center justify-center flex-1">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ 'لوحة التحكم' }}
